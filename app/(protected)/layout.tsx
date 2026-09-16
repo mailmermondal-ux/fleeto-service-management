a950482330ca1c0e76e6d23dcb82755279e720c9
mailmermondal-ux <mailme.rmondal@gmail.com>
2026-09-17 00:07:00 +0530

    Add files via upload

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,12 @@
 import { requireUser } from "@/lib/auth";
 import { Nav } from "@/components/nav";
+
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   await requireUser();
-  return <div className="min-h-screen lg:flex"><Nav/><main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main></div>;
+  return (
+    <div className="min-h-screen lg:flex">
+      <Nav />
+      <main className="min-w-0 flex-1 p-4 md:p-6 lg:p-8">{children}</main>
+    </div>
+  );
 }
